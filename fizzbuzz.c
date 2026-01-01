4001b31c58584453c441e2d14312585fb0473b85
makau <makauwanyoike@gmail.com>
2026-01-01 20:08:55 +0300

    refactor: simplify fizzBuzz function by replacing for loop with while loop

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -8,11 +8,11 @@
 
 void fizzBuzz(void)
 {
-    int i, j;
-
+    int i = 1;
+/*
     for (i = 1; i <= 100; i++)
     {
-        if (i % 5 == 0 && i % 3 == 0)
+        if (i % 15 == 0)
             printf("FizzBuzz\n");
         else if (i % 3 == 0)
             printf("Fizz\n");
@@ -21,9 +21,24 @@ void fizzBuzz(void)
         else
             printf("%d\n", i);
     }
+*/
+    while (i <= 100){
+        //if (i % 5 == 0 && i % 3 == 0)
+        if (i % 15 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 3 == 0)
+            printf("Fizz\n");
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+        else
+            printf("%d\n", i);
+
+        i++;
+    }
 }
 
 int main()
 {
     fizzBuzz();
+    return 0;
 }
